e5f5df888a44349e81cd61f51e729f92599eba20
mmathieucvf <m.mathieu@cvf.ca>
2025-12-19 12:18:31 -0500

    Update service-worker.js2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'panel-scanner-v11';
+const CACHE_NAME = 'panel-scanner-v12';
 const ASSETS = [
   './',
   './index.html',
@@ -18,6 +18,7 @@ self.addEventListener('fetch', event => {
   );
 
 });
+
 
 
 
